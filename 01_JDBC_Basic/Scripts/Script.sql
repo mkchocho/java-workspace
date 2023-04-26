7787a027539912b20ba45d624b9fd37bcbee612a
mkchocho <mkchocho@naver.com>
2023-04-26 17:43:04 +0900

    0426_1742

--- 01_JDBC_Basic/Scripts/Script.sql
+++ 01_JDBC_Basic/Scripts/Script.sql
@@ -43,3 +43,29 @@ SELECT * FROM TEST;
 
 COMMIT;
 ​
+
+
+
+/* Book 데이터 생성 */
+DROP TABLE Book; 
+CREATE TABLE Book (
+bookid NUMBER(2) PRIMARY KEY,
+bookname VARCHAR2(40),
+publisher VARCHAR2(40),
+price NUMBER(8)
+);
+ 
+ /* Book 데이터 생성 */
+INSERT INTO Book VALUES(1, '축구의 역사', '굿스포츠', 7000);
+INSERT INTO Book VALUES(2, '축구아는 여자', '나무수', 13000);
+INSERT INTO Book VALUES(3, '축구의 이해', '대한미디어', 22000);
+INSERT INTO Book VALUES(4, '골프 바이블', '대한미디어', 35000);
+INSERT INTO Book VALUES(5, '피겨 교본', '굿스포츠', 8000);
+INSERT INTO Book VALUES(6, '역도 단계별기술', '굿스포츠', 6000);
+INSERT INTO Book VALUES(7, '야구의 추억', '이상미디어', 20000);
+INSERT INTO Book VALUES(8, '야구를 부탁해', '이상미디어', 13000);
+INSERT INTO Book VALUES(9, '올림픽 이야기', '삼성당', 7500);
+INSERT INTO Book VALUES(10, 'Olympic Champions', 'Pearson', 13000);
+
+commit;
+select * from book;
